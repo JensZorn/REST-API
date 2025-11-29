b2ad6c9a80f6b933e41e60f037574b790e5f7ddc
jens <jens@unter-segeln.net>
2025-11-29 15:57:28 +0100

    Added ServerTimestamp Class, deleted WeatherForecast example

---
+++ Controllers/TimestampController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace REST_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimestampController
+    {
+        [HttpGet(Name = "GetTimestamp")]
+        public ServerTimestamp Get()
+        {
+            return new ServerTimestamp
+            {
+                Timestamp = DateTime.Now
+            };
+        }
+    }
+}
---
+++ ServerTimestamp.cs
@@ -0,0 +1,7 @@
+﻿namespace REST_API
+{
+    public class ServerTimestamp
+    {
+        public DateTime Timestamp { get; set; }
+    }
+}
