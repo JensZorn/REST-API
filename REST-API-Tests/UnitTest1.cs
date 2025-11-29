de5241643ab0f2d7aeafde15cf285eb839da1f9e
jens <jens@unter-segeln.net>
2025-11-29 16:04:03 +0100

    Ordner Struktur angepasst, damit Unit-Tests implementiert werden können

---
+++ REST-API-Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace REST_API_Tests
+{
+    [TestFixture]
+    public class RESTAPITests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
